97e00701810f5b2eecb0547d48514cab21a623ab
PriestofTuna <lyttekk@yahoo.com>
2015-11-02 03:31:40 -0500

    Update Exercise.java

--- Exercise/src/stringvars/Exercise.java
+++ Exercise/src/stringvars/Exercise.java
@@ -1,21 +1,79 @@
-package stringvars;
-import java.util.Scanner;
-public class Exercise {
-
-	/**
-	 * 
-	 * 
-	 */
-	public static void main(String[] args) {
+public class Account {
+	private String fiName;
+	private String laName;
+	private String user;
+	private String email;
+	private String firmPassword;
+	private String accountType;
+	private String password;
+public Account(String fiName, String laName, String user, 
+		String email, String firmPassword,String accountType, String password) {
+	this.setFiName(fiName);
+	this.setLaName(laName);
+	this.setUser(user);
+	this.setEmail(email);
+	this.setFirmPassword(firmPassword);
+	this.setPassword(password);
+	this.setAccountType(accountType);
 	
-		Scanner keyboard = new Scanner(System.in);
-	    //instantiation
-		String username;
-		System.out.println("Enter your username here");
-		username = keyboard.next();
-	String password;
-	System.out.println("Enter your password here");
-	password = keyboard.next();
-	System.out.println("Hello " + username + ", Welcome to CSC200 class! and Your password is " + password + ".");
 }
-		}
+
+	public void setPassword(String Password) {
+		this.password = Password;
+	}
+	public String getpassword() {
+		return password;
+	}
+
+public void setAccountType(String accountType2) {
+	this.accountType = accountType2;
+	
+}
+public String getAccountType() {
+	return accountType;
+}
+//end of accountType
+public void setFirmPassword(String firmPassword2) {
+	this.firmPassword = firmPassword2;
+	
+}
+public String getFirmPassword() {
+	return firmPassword;
+	//end of confirmed password
+}
+public void setEmail(String email2) {
+	this.email = email2;
+	
+}
+public String getEmail() {
+	return email;
+}
+//end of email
+public void setUser(String user2) {
+	this.user=(user2);
+	user2.compareToIgnoreCase(user);
+	
+}
+public String getUser() {
+	return user;
+}
+public void setLaName(String laName2) {
+	this.laName=laName2;
+	
+}
+public String getlaName() {
+	return laName;
+}
+public void setFiName(String fiName2) {
+	this.fiName=fiName2;
+}
+public String getfiName() {
+	return fiName;
+}
+public String toString() {
+	return this.fiName + " " + this.laName +" " + this.email + this.user +" " + this.firmPassword +
+			" " + this.password;
+	
+}
+
+}
